88374d06c1a4308c2f0f360f2f98d631d5487344
Gen0s-Dev <33145738+Gen0s-Dev@users.noreply.github.com>
2018-04-26 14:35:14 +0200

    cpt ok

--- estimate-cost-wp.php
+++ estimate-cost-wp.php
@@ -21,34 +21,75 @@ if ( ! defined ( 'ABSPATH' ) )
     }
 
 class EstimateCostPlugin
+{
+    function __construct()
     {
-        function activation ()
-            {
-                // Génerer CPT
-                // flush rewrite rules
-            }
+        add_action( 'init', array ( $this , 'custom_post_type' ) );
 
-        function desactivation ()
-            {
-                // flush rewrite rules
-            }
-        
-        function desinstaller ()
-            {
-                // Supprimer CPT
-                // Supprimer tous les données de l'extension de la bdd.
-            }
-        
-        function custom_post_type ()
-            {
-                register_post_type();
-            }
     }
+
+    function activation ()
+    {
+        // Génerer CPT
+        $this->custom_post_type();
+        // flush rewrite rules
+        flush_rewrite_rules();
+    }
+
+    function desactivation ()
+    {
+        // flush rewrite rules
+        flush_rewrite_rules();
+    }
+
+    function desinstaller ()
+    {
+        // Supprimer CPT
+        // Supprimer tous les données de l'extension de la bdd.
+    }
+        
+    function custom_post_type ()
+    {
+        $labels = array(
+            'name'                  => 'Devis',
+            'singular_name'         => 'Devis',
+            'add_new'               => 'Créer un devis',
+            'all_items'             => 'Liste des devis',
+            'add_new_item'          => 'Créer un devis',
+            'edit_item'             => 'Edition devis',
+            'new_item'              => 'Nouveau devis',
+            'view_item'             => 'Visualiser devis',
+            'search_item'           => 'Chercher devis',
+            'not_found'             => 'Devis non trouvé',
+            'not_found_in_trash'    => 'Devis non trouvé dans la corbeille',
+            );
+
+        $args = array (
+            'labels'                => $labels,
+            'public'                => false,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'has_archive'           => false,
+            'publicly_queryable'    => true,
+            'query_var'             => true,
+            'rewrite'               => false,
+            'capability_type'       => 'page',
+            'hierarchical'          => false,
+            'supports'              => array ('title','editor','revision','custom-fiels'),
+            'taxonomies'            => array (''),
+            'menu_position'         => 55,
+            'menu_icon'             => 'dashicons-clipboard',
+            'exclude_from_search'   => true,
+            );
+
+        register_post_type('devis', $args );
+    }
+}
  
 if (class_exists('EstimateCostPlugin'))
-    {
-        $estimatecostPlugin = new EstimateCostPlugin();
-    }
+{
+    $estimatecostPlugin = new EstimateCostPlugin();
+}
 
 //Activation
 
@@ -60,6 +101,8 @@ register_deactivation_hook( __FILE__ ,array($estimatecostPlugin,'desactivation')
 
 //Désinstaller
 
+
+
 // add_action ('admin_menu','Addmenu');
 
 //         function Addmenu()
@@ -68,3 +111,4 @@ register_deactivation_hook( __FILE__ ,array($estimatecostPlugin,'desactivation')
 //                 dashicons-clipboard','55');
 //                 add_submenu_page('estimate-cost','Liste des devis','Liste des devis','4','list-estimate-cost','BackendPageListEstimate');
 //             }
+
